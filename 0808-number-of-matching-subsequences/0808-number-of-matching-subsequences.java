ba1cf4fa258eb90d8014c4631995ec4b3df67166
Dinesh Reddy <dineshreddy2295@gmail.com>
2025-04-10 07:09:04 +0530

    Time: 35 ms (97.23%), Space: 45.6 MB (85.21%) - LeetHub

---
+++ 0808-number-of-matching-subsequences/0808-number-of-matching-subsequences.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int numMatchingSubseq(String s, String[] words) {
+
+        Map<String,Integer> mpp = new HashMap<>();
+
+        for(String str : words){
+
+            mpp.put(str,mpp.getOrDefault(str,0) + 1);
+        }
+
+        int ans = 0;
+
+        char ch[] = s.toCharArray();
+
+        for(String str : mpp.keySet()){
+
+            char temp[] = str.toCharArray();
+
+            int i = 0,j = 0;
+
+            while(i < ch.length && j < temp.length){
+
+                if(ch[i] == temp[j]){
+
+                    i++;
+
+                    j++;
+                }
+                else{
+                    i++;
+                }
+            }
+            if(j == temp.length){
+
+                ans += mpp.get(str);
+            }
+        }
+        return ans;
+        
+    }
+}
