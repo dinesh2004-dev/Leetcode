cb806c7ccf586e72775f334ceec4d3a0f73c98c4
Dinesh Reddy <dineshreddy2295@gmail.com>
2025-06-30 18:27:09 +0530

    Time: 12 ms (99.45%), Space: 45.5 MB (70.15%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -3,17 +3,17 @@ class Solution {
         int n = nums.length;
         int count = 0;
         Arrays.sort(nums);
-        for(int i = 0; i < n; i++){
-            int min = nums[i];
-            int max = nums[i];
-            for(int j = i + 1; j < n;j++){
-                min = Math.min(min,nums[j]);
-                max = Math.max(max,nums[j]);
-                 if((max - min) == 1 ){
-                    count = Math.max(count,j - i + 1);
-                 }
+        int left = 0,right = 1;
+        while(right < n){
+            int diff = nums[right] - nums[left];
+            if(left < right && diff > 1){
+                left++;
+                diff = nums[right] - nums[left];
             }
-           
+            if(diff == 1){
+                count = Math.max(count,right - left + 1);
+            }
+            right++;
         }
         return count;
     }
