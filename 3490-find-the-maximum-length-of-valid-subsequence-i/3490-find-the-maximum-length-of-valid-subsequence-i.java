d8c7c22dd6d591ed6ce3aa497322ebe72c2ef719
Dinesh Reddy <dineshreddy2295@gmail.com>
2025-07-16 11:27:54 +0530

    Time: 27 ms (5.74%), Space: 63.5 MB (13.93%) - LeetHub

--- 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.java
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.java
@@ -1,33 +1,30 @@
 class Solution {
-    public int solve(int[] nums,int ind,int n,int even,int[][] dp){
-        if(ind == n){
-            return 0;
-        }
-        if(dp[ind][even] != -1){
-            return dp[ind][even];
+   
+    public int maximumLength(int[] nums) {
+       int n = nums.length;
+        int[][] dp = new int[n + 1][2];
+        
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int even = 0; even <= 1; even++) {
+                int notPick = dp[ind + 1][even];
+                int pick = 0;
+                
+                if ((nums[ind] % 2 == 0 && even == 0) || (nums[ind] % 2 != 0 && even == 1)) {
+                    pick = 1 + dp[ind + 1][(nums[ind] % 2 == 0) ? 1 : 0];
+                }
+                
+                dp[ind][even] = Math.max(pick, notPick);
+            }
         }
         
-        int notPick = solve(nums,ind + 1,n,even,dp);
-        int pick = 0;
-       if((nums[ind] % 2 == 0 && even == 0) || (nums[ind] % 2 != 0 && even == 1)){
-        pick = 1 + solve(nums,ind + 1,n,(nums[ind] % 2 == 0) ? 1 : 0,dp);
-       }
-        return dp[ind][even] = Math.max(pick,notPick);
-
-    }
-    public int maximumLength(int[] nums) {
-        int n = nums.length;
-        int[][] dp = new int[n][2];
-        for(int[] arr : dp){
-            Arrays.fill(arr,-1);
-        }
-        int maxLen = Math.max(solve(nums,0,n,1,dp),solve(nums,0,n,0,dp));
          int even = 0;
         int odd =0;
         for(int i=0;i<n;i++){
             if(nums[i]%2==0)even++;
             else odd++;
         }
-        return Math.max(odd,Math.max(even,maxLen));
+        int alternatingEven = dp[0][0];  // Start with even
+        int alternatingOdd = dp[0][1];   // Start with odd
+        return Math.max(Math.max(odd,even),Math.max( alternatingEven, alternatingOdd));
     }
 }
