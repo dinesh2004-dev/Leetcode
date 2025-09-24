514c9d37c1bd3848eb6cad1760eba7ae065cf1d3
Dinesh Reddy <dineshreddy2295@gmail.com>
2025-09-24 07:52:35 +0530

    Time: 2 ms (84.29%), Space: 45.8 MB (56.47%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,41 +1,18 @@
 class Solution {
-    public boolean solve(int[] nums, int start, Boolean[] memo) {
-        
-        if (start >= nums.length - 1) {
-            return true;
-        }
-        
-        if (memo[start] != null) {
-            return memo[start];
-        }
-        
-        if (nums[start] == 0) {
-            memo[start] = false;
-            return false;
-        }
-        
-        
-        for (int i = 1; i <= nums[start]; i++) {
-            if (solve(nums, start + i, memo)) {
-                memo[start] = true;
-                return true;
-            }
-        }
-        
-        memo[start] = false;
-        return false;
-    }
-    
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        int reachable = 0;
+        
+        int maxReach = 0;
 
-        for(int i = 0; i < n; i++){
-            if(i > reachable){
+        for(int i = 0; i < nums.length; i++){
+
+            if(i > maxReach){
                 return false;
             }
-            reachable = Math.max(reachable,i + nums[i]);
+            maxReach = Math.max(maxReach,nums[i] + i);
+            
         }
+
         return true;
+        
     }
 }
