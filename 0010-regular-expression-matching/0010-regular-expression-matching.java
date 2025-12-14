58509784202b33808569d64ae9c7141311090c14
Dinesh Reddy <dineshreddy2295@gmail.com>
2025-12-14 16:06:24 +0530

    Time: 2 ms (56.97%), Space: 43.8 MB (18.6%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -15,7 +15,7 @@ class Solution {
 
         return true;
     }
-    public boolean solve(String s,String p,int ind1,int ind2){
+    public boolean solve(String s,String p,int ind1,int ind2,boolean[][] dp){
 
         if(ind1 < 0 && ind2 < 0){
 
@@ -31,30 +31,37 @@ class Solution {
             return checkAllStars(p,ind2);
         }
 
+        if(dp[ind1][ind2]){
+
+            return dp[ind1][ind2];
+        }
+
         if(s.charAt(ind1) == p.charAt(ind2) || p.charAt(ind2) == '.'){
 
-            return solve(s,p,ind1 - 1,ind2 - 1);
+            return dp[ind1][ind2] = solve(s,p,ind1 - 1,ind2 - 1,dp);
         }
         if(p.charAt(ind2) == '*'){
 
-            boolean zero = solve(s,p,ind1,ind2 - 2);
+            boolean zero = solve(s,p,ind1,ind2 - 2,dp);
 
             boolean multiple = false;
 
             if(p.charAt(ind2 - 1) == s.charAt(ind1) || p.charAt(ind2 - 1) == '.'){
-                multiple = solve(s,p,ind1 - 1,ind2);
+                multiple = solve(s,p,ind1 - 1,ind2,dp);
             }
 
-            return zero || multiple;
+            return dp[ind1][ind2] = zero || multiple;
         }
 
-        return false;
+        return dp[ind1][ind2] = false;
     }
     public boolean isMatch(String s, String p) {
         
         int n1 = s.length() - 1;
         int n2 = p.length() - 1;
 
-        return solve(s,p,n1,n2);
+        boolean[][] dp = new boolean[n1+1][n2+1];
+
+        return solve(s,p,n1,n2,dp);
     }
 }
