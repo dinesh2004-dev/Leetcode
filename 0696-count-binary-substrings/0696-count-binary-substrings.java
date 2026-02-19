96dca972b85f81160b4b90f4007c8256763f55ec
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-02-19 10:55:32 +0530

    Time: 11 ms (58.7%), Space: 46.5 MB (40.98%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.java
+++ 0696-count-binary-substrings/0696-count-binary-substrings.java
@@ -2,70 +2,24 @@ class Solution {
     public int countBinarySubstrings(String s) {
         
         int cnt = 0;
+        int prev = 0;
+        int curr = 1;
 
-        char[] charStr = s.toCharArray();
+        for(int i = 1; i < s.length(); i++){
 
-        int n = charStr.length;
+            if(s.charAt(i - 1) == s.charAt(i)){
 
-        int onePtr = 0,zeroPtr = 0;
+                curr++;
+            }
+            else{
 
-        for(int i = 0; i < n; i++){
-
-           if(charStr[i] == '0'){
-
-            // System.out.println(i);
-
-                int zeroStart = i;
-
-                while(i < n && charStr[i] == '0'){
-
-                    i++;
-                }
-
-                int oneStart = i;
-
-                while( i < n && charStr[i] == '1'){
-
-                    i++;
-                }
-
-                int lastIdx = i;
-
-                int zeroLen = oneStart - zeroStart;
-                int oneLen = lastIdx - oneStart;
-
-                cnt += Math.min(zeroLen,oneLen);
-
-                i = oneStart - 1;
-           }
-           else{
-
-                System.out.println(i);
-
-                int oneStart = i;
-
-                while(i < n && charStr[i] == '1'){
-
-                    i++;
-                }
-
-                int zeroStart = i;
-
-                while(i < n && charStr[i] == '0'){
-
-                    i++;
-                }
-
-                int lastIdx = i;
-
-                int oneLen = zeroStart - oneStart;
-                int zeroLen = lastIdx - zeroStart;
-
-                i = zeroStart - 1;
-
-                cnt += Math.min(oneLen,zeroLen);
-           }
+                cnt += Math.min(prev,curr);
+                prev = curr;
+                curr = 1;
+            }
         }
+
+        cnt += Math.min(prev,curr);
 
         return cnt;
     }
