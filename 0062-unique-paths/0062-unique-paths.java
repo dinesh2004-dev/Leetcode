6c5a15928ab6c6d48b147fdddd03cb28aa2e0d32
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-02-20 14:21:42 +0530

    Time: 1 ms (14.47%), Space: 42.2 MB (65.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,21 +1,21 @@
 class Solution {
-    private void solve(int row,int col,int m,int n,String path,Set<String> set){
+    // private void solve(int row,int col,int m,int n,String path,Set<String> set){
 
-        if(row > m || row < 1 || col > n || col < 1){
+    //     if(row > m || row < 1 || col > n || col < 1){
 
-            return;
-        }
+    //         return;
+    //     }
 
-        if(row == m && col == n){
+    //     if(row == m && col == n){
 
-            set.add(path);
-            return;
-        }
+    //         set.add(path);
+    //         return;
+    //     }
 
-        solve(row + 1,col,m,n,path+"B",set);
-        solve(row,col + 1,m,n,path+"R",set);
+    //     solve(row + 1,col,m,n,path+"B",set);
+    //     solve(row,col + 1,m,n,path+"R",set);
 
-    }
+    // }
     public int uniquePaths(int m, int n) {
         
         int dp[][] = new int[m+1][n+1];
