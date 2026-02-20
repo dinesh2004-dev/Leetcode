048e56d1301572c1ab83244ef8712338e76bfed4
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-02-20 14:15:47 +0530

    Time: 1 ms (14.47%), Space: 42.2 MB (65.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,33 +1,51 @@
 class Solution {
+    private void solve(int row,int col,int m,int n,String path,Set<String> set){
+
+        if(row > m || row < 1 || col > n || col < 1){
+
+            return;
+        }
+
+        if(row == m && col == n){
+
+            set.add(path);
+            return;
+        }
+
+        solve(row + 1,col,m,n,path+"B",set);
+        solve(row,col + 1,m,n,path+"R",set);
+
+    }
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+        
+        int dp[][] = new int[m+1][n+1];
 
-       
+        for(int i = m; i > 0; i--){
 
-        for(int i = 0; i < m; i++){
+            for(int j = n; j > 0; j--){
 
-            for(int j = 0; j < n; j++){
-
-                if( i== 0 && j== 0){
+                if(i == m && j == n){
 
                     dp[i][j] = 1;
 
                     continue;
                 }
 
-                int up = 0;
-                int left = 0;
-                if(i > 0){
-                    up = dp[i - 1][j];
+                int down = 0,right = 0;
+
+                if(i + 1 <= m){
+
+                    down = dp[i + 1][j];
                 }
-                if(j > 0){
-                 left = dp[i][j - 1];
+                if(j + 1 <= n){
+
+                    right = dp[i][j + 1];
                 }
 
-                dp[i][j] = up + left;
+                dp[i][j] = down + right;
             }
         }
 
-        return dp[m - 1][n - 1];
+        return dp[1][1];
     }
 }
