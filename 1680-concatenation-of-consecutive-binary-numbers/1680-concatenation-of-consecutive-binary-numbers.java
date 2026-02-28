8b8eed39c29ab13a23b87ccab741b7affb68cbcd
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-02-28 10:39:24 +0530

    Time: 24 ms (98.84%), Space: 42.1 MB (98.84%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int concatenatedBinary(int n) {
+        
+        long res = 0;
+        int bits = 0;
+        int mod = (int) 1e9 + 7;
+
+        for(int i = 1; i <= n; i++){
+
+            if((i & (i - 1)) == 0){
+
+                bits++;
+            }
+
+            res = ((res << bits) | i) % mod;
+        }
+
+        return (int)res;
+    }
+}
