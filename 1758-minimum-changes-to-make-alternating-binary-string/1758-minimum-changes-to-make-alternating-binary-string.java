16c8fcf2c53429d7ee807ca0550f66df1d9267ea
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-03-05 07:50:52 +0530

    Time: 3 ms (92.43%), Space: 44.1 MB (18.42%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -4,34 +4,30 @@ class Solution {
         char[] charArray = s.toCharArray();
 
        int minOperations1 = 0;
+        int minOperations2 = 0;
 
-       char expected = '0';
+       char expected1 = '0';
+       char expected2 = '1';
 
        for(char ch : charArray){
 
-            if(expected != ch){
+            if(expected1 != ch){
 
                 minOperations1++;
             }
 
-            expected = (expected == '0') ? '1' : '0';
-       }
+            if(expected2 != ch){
 
-       int minOperation2 = 0;
-
-       expected = '1';
-
-        for(char ch : charArray){
-
-            if(expected != ch){
-
-                minOperation2++;
+                minOperations2++;
             }
 
-            expected = (expected == '0') ? '1' : '0';
+
+            expected1 = (expected1 == '0') ? '1' : '0';
+             expected2 = (expected2 == '0') ? '1' : '0';
        }
 
-       return Math.min(minOperations1,minOperation2);
+    
+       return Math.min(minOperations1,minOperations2);
 
 
     }
