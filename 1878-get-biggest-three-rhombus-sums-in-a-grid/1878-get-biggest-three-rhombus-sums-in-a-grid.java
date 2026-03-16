0f049ea7d669e32b18cfabd60d0d11fb74096111
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-03-16 11:47:07 +0530

    Time: 150 ms (11.76%), Space: 48.4 MB (25%) - LeetHub

--- 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.java
+++ 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.java
@@ -27,23 +27,67 @@ class Solution {
         
         int m = grid.length;
 	   int n = grid[0].length;
+
+       int[][] diag1 = new int[m+1][n+1];
+
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                diag1[i][j] = grid[i-1][j-1] + diag1[i-1][j-1];
+            }
+        }
+
+        int[][] diag2 = new int[m+1][n+2];
+
+        for(int i = 1; i <= m; i++){
+            for(int j = n; j >= 1; j--){
+                diag2[i][j] = grid[i-1][j-1] + diag2[i-1][j+1];
+            }
+        }
 	   
 	
 
       TreeSet<Integer> set = new TreeSet<>(Collections.reverseOrder());
 
-    for(int i = 0; i < m; i++){
+    for(int r = 0; r < m; r++){
 
-        for(int j = 0; j < n; j++){
+        for(int c = 0; c < n; c++){
 
-            int maxSize = Math.min(Math.min(i, m-1-i),
-                               Math.min(j, n-1-j));
+            int maxSize = Math.min(Math.min(r, m-1-r),
+                               Math.min(c, n-1-c));
 
             for(int k = 0; k <= maxSize; k++){
 
-                int sum = findBorderSum(grid, i, j, k);
+                 if(k == 0){
+                    set.add(grid[r][c]);
+                    continue;
+                }
 
-                set.add(sum);
+                  int topRow = r-k;
+                    int bottomRow = r+k;
+                    int leftCol = c-k;
+                    int rightCol = c+k;
+
+                    int sum = 0;
+
+                    // top -> right
+                    sum += diag1[r+1][rightCol+1] - diag1[topRow][c];
+
+                    // right -> bottom
+                    sum += diag2[bottomRow+1][c+1] - diag2[r][rightCol+2];
+
+                    // bottom -> left
+                    sum += diag1[bottomRow+1][c+1] - diag1[r][leftCol];
+
+                    // left -> top
+                    sum += diag2[r+1][leftCol+1] - diag2[topRow][c+2];
+
+                    // remove corner duplicates
+                    sum -= grid[topRow][c];
+                    sum -= grid[r][rightCol];
+                    sum -= grid[bottomRow][c];
+                    sum -= grid[r][leftCol];
+
+                    set.add(sum);
             }
     } 
     }
