038df051c9a2224b90e08b01e8b66080aa6ab7d3
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-04-16 12:40:54 +0530

    Time: 27 ms (26.61%), Space: 48 MB (24.66%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,52 +1,26 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        
         int n = arr.length;
-        
-        
-        int low = 0, high = n - 1, pos = -1;
-        
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            
-            if (arr[mid] < x) {
-                pos = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+        int[][] temp = new int[n][2];
+
+        for(int i = 0; i < n; i++){
+
+            temp[i][0] = Math.abs(arr[i] - x);
+            temp[i][1] = i;
         }
-        
-       
-        int left = pos;
-        int right = pos + 1;
-        
+
+        Arrays.sort(temp,(u,v) -> u[0] - v[0]);
+
         List<Integer> res = new ArrayList<>();
-        
-       
-        while (res.size() < k) {
-            if (left < 0) {
-                
-                res.add(arr[right]);
-                right++;
-            } else if (right >= n) {
-                
-                res.add(arr[left]);
-                left--;
-            } else {
-                
-                int leftDiff = Math.abs(arr[left] - x);
-                int rightDiff = Math.abs(arr[right] - x);
-                
-                if (leftDiff < rightDiff || (leftDiff == rightDiff && arr[left] < arr[right])) {
-                    res.add(arr[left]);
-                    left--;
-                } else {
-                    res.add(arr[right]);
-                    right++;
-                }
+
+        for(int i = 0; i < n ; i++){
+            if(res.size() == k){
+                Collections.sort(res);
+                return res;
             }
+            res.add(arr[temp[i][1]]);
         }
-        
         Collections.sort(res);
         return res;
     }
