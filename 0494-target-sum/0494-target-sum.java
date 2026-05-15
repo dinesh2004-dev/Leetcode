9c80182c4d43707c0a316284c1f89de074bdf390
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-05-15 14:05:53 +0530

    Time: 6 ms (71.04%), Space: 45.1 MB (39.26%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,27 +1,52 @@
 class Solution {
     private int targetSum(int ind,int[] nums,int target){
 
-         if(ind == nums.length){
+        //  if(ind == nums.length){
 
-            if(target == 0){
+        //     if(target == 0){
 
-                return 1;
-            }
+        //         return 1;
+        //     }
 
-            return 0;
-        }
+        //     return 0;
+        // }
        
 
-        int notPick = targetSum(ind + 1,nums,target);
+        // int notPick = targetSum(ind + 1,nums,target);
 
-        int pick = 0;
+        // int pick = 0;
 
-        if(target - nums[ind] >= 0){
+        // if(target - nums[ind] >= 0){
 
-            pick = targetSum(ind + 1,nums,target - nums[ind]);
+        //     pick = targetSum(ind + 1,nums,target - nums[ind]);
+        // }
+
+        // return pick + notPick;
+
+        int n = nums.length;
+
+        int[][] dp = new int[n + 1][target + 1];
+
+        dp[n][0] = 1;
+
+        for(int i = n - 1; i >= 0; i--){
+
+            for(int tar = target; tar >= 0; tar--){
+
+                int notPick = dp[i + 1][tar];
+                
+                int pick = 0;
+
+                if(tar - nums[i] >= 0){
+
+                    pick = dp[i + 1][tar - nums[i]];
+                }
+
+                dp[i][tar] = pick + notPick;
+            }
         }
 
-        return pick + notPick;
+        return dp[0][target];
     }
     public int findTargetSumWays(int[] nums, int target) {
         
