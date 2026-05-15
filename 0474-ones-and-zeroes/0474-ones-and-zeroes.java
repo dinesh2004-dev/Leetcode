a7bd57d5c54364990091681bb94e4f32499c4aa4
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-05-15 18:52:32 +0530

    Time: 74 ms (15.79%), Space: 107.2 MB (7.13%) - LeetHub

---
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.java
@@ -0,0 +1,66 @@
+class Solution {
+    private int[] countOnesAndZeros(String s){
+
+        int ones = 0,zeros = 0;
+
+        for(char ch : s.toCharArray()){
+
+            if(ch == '1'){
+
+                ones++;
+            }
+            else{
+
+                zeros++;
+            }
+        }
+
+        return new int[]{zeros,ones};
+    }
+    private int solve(int ind,String[] strs,int m,int n,int[][][] dp){
+
+        if(strs.length == ind){
+
+            return 0;
+        }
+
+        if(dp[ind][m][n] != -1){
+
+            return dp[ind][m][n];
+        }
+
+        int notPick = solve(ind + 1,strs,m,n,dp);
+
+        int[] onesAndZeros = countOnesAndZeros(strs[ind]);
+
+        int zeros = onesAndZeros[0];
+        int ones = onesAndZeros[1];
+
+        int pick = Integer.MIN_VALUE;
+
+        if(m >= zeros && n >= ones){
+
+            pick = 1 + solve(ind + 1,strs,m - zeros,n - ones,dp);
+
+        }
+
+        return dp[ind][m][n] = Math.max(pick,notPick);
+      
+    }
+    public int findMaxForm(String[] strs, int m, int n) {
+
+        int len = strs.length;
+
+        int[][][] dp = new int[len][m + 1][n + 1];
+
+        for(int[][] d1 : dp){
+
+            for(int[] d2 : d1){
+
+                Arrays.fill(d2,-1);
+            }
+        }
+        
+        return solve(0,strs,m,n,dp);
+    }
+}
