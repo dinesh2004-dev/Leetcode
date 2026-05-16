ba1896047639a6147f7a71e1336f5af39df106b5
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-05-16 11:52:10 +0530

    Time: 6 ms (86.41%), Space: 46.1 MB (54.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,30 +1,46 @@
 class Solution {
-    // public int solve(int[] nums,int ind,int prev_ind,int n){
-    //     if(ind == n){
-    //         return 0;
-    //     }
+    private int lowerBound(List<Integer> list,int key){
 
-    //     int len = solve(nums,ind + 1,prev_ind,n);
-    //     if(prev_ind == - 1 || nums[prev_ind] < nums[ind]){
-    //         len = Math.max(len,1 + solve(nums,ind + 1,ind,n));
-    //     }
-    //     return len;
-    // }
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int dp[][] = new int[n + 1][n + 1];
-        for(int i = n - 1; i >= 0; i--){
-            for(int j = i - 1; j >= -1; j--){
-                
-                    int len = dp[i + 1][j + 1];
-                    if(j == -1 || nums[j] < nums[i]){
-                        len = Math.max(len, 1 + dp[i + 1][i + 1]);
-                    }
-                    dp[i][j + 1] = len;
-                }
+        int low = 0,high = list.size() - 1;
+
+        while(low <= high){
+
+            int mid = low + ( high - low) / 2;
+
+            if(list.get(mid) >= key){
+
+                high = mid - 1;
             }
-            return dp[0][-1 + 1];
+            else{
+
+                low = mid + 1;
+            
+            }
         }
-        
+
+        return low;
     }
-    
+    public int lengthOfLIS(int[] nums) {
+        
+        List<Integer> list = new ArrayList<>();
+
+        int n = nums.length;
+
+        list.add(nums[0]);
+
+        for(int i = 1; i < n; i++){
+
+            if(list.get(list.size() - 1) < nums[i]){
+
+                list.add(nums[i]);
+            }
+            else{
+
+                int idx = lowerBound(list,nums[i]);
+                list.set(idx,nums[i]);
+            }
+        }
+
+        return list.size();
+    }
+}
