0c07d94c7abadd5c4ebce71f4f04571a9a67b4a4
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-05-16 08:32:48 +0530

    Time: 424 ms (5.13%), Space: 286.9 MB (5.3%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.java
+++ 0879-profitable-schemes/0879-profitable-schemes.java
@@ -27,20 +27,59 @@ class Solution {
     }
     public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
 
+        // int len = group.length;
+
+        // int profitSum = Arrays.stream(profit).sum();
+
+        // int[][][] dp = new int[len][n + 1][profitSum + 1];
+
+        // for(int[][] d1 : dp){
+        //     for(int[] d2 : d1){
+
+        //         Arrays.fill(d2,-1);
+        //     }
+        // }
+
+        // return solve(0,n,minProfit,0,group,profit,dp);
+
         int len = group.length;
 
         int profitSum = Arrays.stream(profit).sum();
 
-        int[][][] dp = new int[len][n + 1][profitSum + 1];
+        int[][][] dp = new int[len  + 1][n + 1][profitSum + 1];
 
-        for(int[][] d1 : dp){
-            for(int[] d2 : d1){
+       for(int j = 0; j <= n; j++){
 
-                Arrays.fill(d2,-1);
+        for(int k = minProfit; k <= profitSum; k++){
+
+            dp[len][j][k] = 1;
+          }
+       }
+
+       for(int ind = len - 1; ind >= 0; ind--){
+
+            for(int j = 0; j <= n; j++){
+
+                for(int currProfit = 0; currProfit <= profitSum; currProfit++){
+
+                    int notPick =dp[ind + 1][j][currProfit];
+
+                    int pick = 0;
+
+                    if(j >= group[ind]){
+
+                         int newProfit = Math.min(currProfit + profit[ind], profitSum);
+                        pick = dp[ind + 1][j - group[ind]][newProfit];
+                    }
+
+                    dp[ind][j][currProfit] = (pick + notPick) % 1000000007;
+                }
             }
-        }
+       }
 
-        return solve(0,n,minProfit,0,group,profit,dp);
+       return dp[0][n][0];
+
+
         
     }
 }
