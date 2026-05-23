23e17f58c5f4eb238c5a5cab34c74f2f2df33dae
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-05-23 11:34:17 +0530

    Time: 65 ms (11.71%), Space: 107.6 MB (5.05%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int solve(int ind1,int ind2,String str1,String str2,int[][] dp){
+    public int memo(int ind1,int ind2,String str1,String str2,int[][] dp){
 
         if(ind1 < 0 || ind2 < 0){
 
@@ -13,11 +13,36 @@ class Solution {
 
         if(str1.charAt(ind1) == str2.charAt(ind2)){
 
-            return dp[ind1][ind2] = 1 + solve(ind1 - 1,ind2 - 1,str1,str2,dp);
+            return dp[ind1][ind2] = 1 + memo(ind1 - 1,ind2 - 1,str1,str2,dp);
         }
 
-        return dp[ind1][ind2] = Math.max(solve(ind1 - 1,ind2,str1,str2,dp),
-                        solve(ind1,ind2 - 1,str1,str2,dp));
+        return dp[ind1][ind2] = Math.max(memo(ind1 - 1,ind2,str1,str2,dp),
+                        memo(ind1,ind2 - 1,str1,str2,dp));
+    }
+    private int tabulation(String str1,String str2){
+
+        int m = str1.length();
+        int n = str2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for(int i = m - 1; i >= 0; i--){
+
+            for(int j = n - 1; j >= 0; j--){
+
+                if(str1.charAt(i) == str2.charAt(j)){
+
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                }
+                else{
+
+                    dp[i][j] = Math.max(dp[i + 1][j],dp[i][j + 1]);
+                }
+          
+            }
+        }
+
+        return dp[0][0];
     }
     public int longestPalindromeSubseq(String s) {
 
@@ -32,7 +57,8 @@ class Solution {
             Arrays.fill(d,-1);
         }
 
-        return solve(n - 1,n - 1,s,rev,dp);
+        // return memo(n - 1,n - 1,s,rev,dp);
+        return tabulation(s,rev);
         
     }
 }
