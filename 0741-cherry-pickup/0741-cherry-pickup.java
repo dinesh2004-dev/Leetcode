f48796e2d7a739f81509ac72e066405dcf3df21d
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-05-24 20:34:34 +0530

    Time: 23 ms (33.57%), Space: 48.1 MB (31.15%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.java
+++ 0741-cherry-pickup/0741-cherry-pickup.java
@@ -1,8 +1,11 @@
 class Solution {
    private static int m,n;
-    private int solve(int[][] grid,int r1,int c1,int r2,int c2,int[][][][] dp){
+    private int solve(int[][] grid,int t,int r1,int r2,int[][][] dp){
 
-        if(r1 >= m || c1 >= n || r2 >= m || c2>= n || grid[r1][c1] == -1 || 
+        int c1 = t - r1;
+        int c2 = t - r2;
+
+        if(r1 >= m || c1 >= n || r2 >= m || c2 >= n || grid[r1][c1] == -1 || 
             grid[r2][c2] == -1){
 
                 return Integer.MIN_VALUE;
@@ -18,9 +21,9 @@ class Solution {
             return grid[r2][c2];
         }
 
-         if(dp[r1][c1][r2][c2] != -1){
+         if(dp[t][r1][r2] != -1){
 
-            return dp[r1][c1][r2][c2];
+            return dp[t][r1][r2];
         }
 
         int cherries = 0;
@@ -34,35 +37,35 @@ class Solution {
             cherries += grid[r1][c1] + grid[r2][c2];
         }
 
-        int down = solve(grid,r1 + 1,c1,r2 + 1,c2,dp);
-        int downRight = solve(grid,r1 + 1,c1,r2,c2 + 1,dp);
-        int rightDown = solve(grid,r1,c1 + 1,r2 + 1,c2,dp);
-        int right = solve(grid,r1,c1 + 1,r2,c2 + 1,dp);
+        int down = solve(grid,t + 1,r1 + 1,r2 + 1,dp);
+        int downRight = solve(grid,t + 1,r1 + 1,r2,dp);
+        int rightDown = solve(grid,t + 1,r1,r2 + 1,dp);
+        int right = solve(grid,t + 1,r1,r2,dp);
 
         cherries += Math.max(Math.max(down,downRight),Math.max(rightDown,right));
 
-        return dp[r1][c1][r2][c2] = cherries;
+        return dp[t][r1][r2] = cherries;
     }
     public int cherryPickup(int[][] grid) {
         
         m = grid.length;
         n = grid[0].length;
 
-        int[][][][] dp = new int[m][n][m][n];
+        System.out.println(m +" "+n);
 
-        for(int[][][] d1 : dp){
+        int[][][] dp = new int[2 * n - 1][m][m];
 
-            for(int[][] d2 : d1){
+        for(int[][] d1 : dp){
 
-                for(int[] d3 : d2){
+            for(int[] d2 : d1){
 
-                    Arrays.fill(d3,-1);
-                }
+                Arrays.fill(d2,-1);
+                
             }
 
         }
 
-        int res = solve(grid,0,0,0,0,dp);
+        int res = solve(grid,0,0,0,dp);
 
         return Math.max(0,res);
     }
