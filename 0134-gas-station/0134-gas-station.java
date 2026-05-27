82c1ed83c03034d080e5c7b497405a1a3f5fba9c
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-05-27 07:24:05 +0530

    Time: 8 ms (4.99%), Space: 119.2 MB (80.92%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,16 +1,17 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
+        
         int n = gas.length;
 
-        int totalGas = Arrays.stream(gas).sum();
-        int totalCost = Arrays.stream(cost).sum();
+        int gasSum = Arrays.stream(gas).sum();
+        int costSum = Arrays.stream(cost).sum();
 
-        if(totalGas < totalCost){
+        if(costSum > gasSum){
 
             return -1;
         }
 
-        int currGas = 0,start = 0;
+        int currGas = 0,startIdx = 0;
 
         for(int i = 0; i < n; i++){
 
@@ -19,10 +20,10 @@ class Solution {
             if(currGas < 0){
 
                 currGas = 0;
-
-                start = i + 1;
+                startIdx = i + 1;
             }
         }
-        return start;
+
+        return startIdx;
     }
 }
