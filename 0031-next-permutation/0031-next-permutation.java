1a4fe9f57ffa52ed091c9a257cfdf89e4cd2ff35
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-05-30 10:36:33 +0530

    Time: 0 ms (100%), Space: 44.7 MB (31.06%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,38 +1,48 @@
 class Solution {
-    public void reverse(int[] nums,int i,int j){
-        while(i <= j){
-            swap(nums,i,j);
-            i++;
-            j--;
+    private void reverse(int[] nums,int left,int right){
+
+        while(left <= right){
+
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
         }
     }
-    public void swap(int[] nums,int i,int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
     public void nextPermutation(int[] nums) {
+        
         int n = nums.length;
+
         int idx = -1;
+
         for(int i = n - 2; i >= 0; i--){
+
             if(nums[i] < nums[i + 1]){
+
                 idx = i;
                 break;
             }
         }
-        
+
         if(idx == -1){
+
             reverse(nums,0,n - 1);
             return;
         }
 
-        for(int i = n - 1; i >= idx; i--){
-            if(nums[i] > nums[idx]){
-                swap(nums,i,idx);
+        for(int i = n - 1; i >= 0; i--){
+
+            if(nums[idx] < nums[i]){
+
+                int temp = nums[idx];
+                nums[idx] = nums[i];
+                nums[i] = temp;
                 break;
             }
         }
+
         reverse(nums,idx + 1,n - 1);
-        
     }
 }
