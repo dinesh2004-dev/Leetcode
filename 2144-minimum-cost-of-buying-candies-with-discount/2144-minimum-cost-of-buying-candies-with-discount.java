5d41df8dafb84fe15b7b2c13f40d3d3dcb64a751
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-06-01 09:43:42 +0530

    Time: 6 ms (41.78%), Space: 44.4 MB (69.64%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
@@ -1,26 +1,11 @@
 class Solution {
-    private void reverse(int[] cost){
-
-        int l = 0,r = cost.length - 1;
-
-        while(l <= r){
-
-            int temp = cost[l];
-            cost[l] = cost[r];
-            cost[r] = temp;
-
-            l++;
-            r--;
-        }
-    }
+   
     public int minimumCost(int[] cost) {
         
         int n = cost.length;
         
         Arrays.sort(cost);
-
-        // reverse(cost);
-
+        
         int totalCost = 0;
 
         int free = 0;
