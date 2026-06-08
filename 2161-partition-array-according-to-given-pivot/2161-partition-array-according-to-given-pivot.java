79564e92b84c03c7ac8e0ec8778da79168acb151
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-06-08 07:27:11 +0530

    Time: 5 ms (73.56%), Space: 159.9 MB (61.37%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -3,40 +3,29 @@ class Solution {
         
         int n = nums.length;
 
-        PriorityQueue<int[]> pq1 = new PriorityQueue<>((x,y) -> x[1] - y[1]);
-        PriorityQueue<int[]> pq2 = new PriorityQueue<>((x,y) -> x[1] - y[1]);
-
-        for(int i = 0; i < n; i++){
-
-            if(nums[i] > pivot){
-
-                pq2.add(new int[]{nums[i],i});
-            }
-            else if(nums[i] < pivot){
-
-                pq1.add(new int[]{nums[i],i});
-            }
-        }
-
-        int pivots = n - (pq1.size() + pq2.size());
+        int i = 0,j = n - 1;
+        int i1 = 0,j1 = n - 1;
 
         int[] res = new int[n];
 
-        int i = 0;
+        while(i < n && j >= 0){
 
-        while(!pq1.isEmpty()){
+            if(nums[i] < pivot){
 
-            res[i++] = pq1.poll()[0];
+                res[i1++] = nums[i];
+            }
+            if(nums[j] > pivot){
+
+                res[j1--] = nums[j];
+            }
+
+            i++;
+            j--;
         }
 
-        for(int j = 0; j < pivots; j++){
+        for(int k = i1; k <= j1; k++){
 
-            res[i++] = pivot;
-        }
-
-        while(!pq2.isEmpty()){
-
-            res[i++] = pq2.poll()[0];
+            res[k] = pivot;
         }
 
         return res;
