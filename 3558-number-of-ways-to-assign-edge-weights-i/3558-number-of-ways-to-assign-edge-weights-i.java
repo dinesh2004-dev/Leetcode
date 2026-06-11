98535c812953cf19642c336eade4a0ba84961e54
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-06-11 10:43:50 +0530

    Time: 186 ms (28.57%), Space: 303.7 MB (14.28%) - LeetHub

---
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.java
@@ -0,0 +1,63 @@
+class Solution {
+
+    private static int MOD = (int) 1e9+7;
+    
+    private int dfs(int node,int parent,Map<Integer,List<Integer>> adj){
+
+        int depth = 0;
+        for(int neighbour : adj.getOrDefault(node,new ArrayList<>())){
+
+            if(neighbour != parent){
+
+                depth = Math.max(depth,1 + dfs(neighbour,node,adj));
+            }
+        }
+
+        return depth;
+
+    }
+    private long modPow(long base, int exp) {
+
+        long ans = 1;
+
+        while (exp > 0) {
+
+            if ((exp & 1) == 1) {
+                ans = (ans * base) % MOD;
+            }
+
+            base = (base * base) % MOD;
+            exp >>= 1;
+        }
+
+        return ans;
+    }
+    public int assignEdgeWeights(int[][] edges) {
+        
+        Map<Integer,List<Integer>> adj = new HashMap<>();
+
+        for(int[] edge : edges){
+
+            int u = edge[0];
+            int v = edge[1];
+
+            if(!adj.containsKey(u)){
+
+                adj.put(u,new ArrayList<>());
+            }
+             if(!adj.containsKey(v)){
+
+                adj.put(v,new ArrayList<>());
+            }
+
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+
+            
+        }
+
+        int depth = dfs(1,-1,adj);
+
+        return (int) modPow(2,depth - 1);
+    }
+}
