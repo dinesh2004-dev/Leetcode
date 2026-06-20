ffda65e40110f1e7287c0be018921bad07d17fc7
Dinesh Reddy <dineshreddy2295@gmail.com>
2026-06-20 15:11:29 +0530

    Time: 62 ms (17.65%), Space: 123.4 MB (85.29%) - LeetHub

---
+++ 1840-maximum-building-height/1840-maximum-building-height.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int maxBuilding(int n, int[][] restrictions) {
+        
+        List<int[]> list = new ArrayList<>();
+
+        for(int[] restriction : restrictions){
+
+            list.add(new int[]{restriction[0],restriction[1]});
+        }
+
+        list.add(new int[]{1,0});
+
+        list.sort((x,y) -> Integer.compare(x[0],y[0]));
+
+        if(list.get(list.size() - 1)[0] != n){
+
+            list.add(new int[]{n,n - 1});
+        }
+
+        int m = list.size();
+
+        for(int i = 1; i < m; i++){
+
+            int dist = list.get(i)[0] - list.get(i - 1)[0];
+
+            list.get(i)[1] = Math.min(list.get(i)[1],list.get(i - 1)[1] + dist);
+        }
+
+        for(int i = m - 2; i >= 0; i--){
+
+            int dist = list.get(i + 1)[0] - list.get(i)[0];
+
+            list.get(i)[1] = Math.min(list.get(i)[1],list.get(i + 1)[1] + dist);
+        }
+
+        long ans = 0;
+
+        for(int i = 1; i < m; i++){
+
+            long x1 = list.get(i - 1)[0];
+            long h1 = list.get(i - 1)[1];
+
+            long x2 = list.get(i)[0];
+            long h2 = list.get(i)[1];
+
+            long dist = x2 - x1;
+            ans = Math.max(ans,(h1 + h2 + dist) / 2);
+        }
+
+        return (int) ans;
+    }
+}
